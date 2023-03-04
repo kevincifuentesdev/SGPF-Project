e9bbb313b1d9e72a80a8e87d61940dc52190fba2
unknown <Sena@MEDAPRCSGFSD811.FormacionSena.RED>
2023-03-03 20:58:30 -0500

    maquetacion html fase instructor

---
+++ faseInstructor.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/faseInstructor.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<!-- Fonts -->
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400;700;800&display=swap" rel="stylesheet">
+<title>Document</title>
+
+</head>
+<body>
+    <section>
+        <h2>Datos Fase por instructor</h2>
+        <hr>
+        <input type="search" class="busqueda" placeholder="Buscar por nombre o documento" size="28px">
+        <br>
+        <a href="#">Nueva Fase por instructor</a>
+        <br>
+        <main>
+            <table>
+                <tr>
+                    Fase
+                    <td>planeacion</td>
+                    <td>Motos</td>
+                    <td>Carlos Gomez</td>
+                    <td><a href="#">Ver detalle/editar/eliminar</a></td>
+                </tr>
+                <tr>
+                    Proyecto
+                </tr>
+                <tr>
+                    instructor
+                <tr>  
+                <tr>
+    
+                </tr>
+            </table>
+        </main>
+    </section>
+    
+</body>
+</html>
