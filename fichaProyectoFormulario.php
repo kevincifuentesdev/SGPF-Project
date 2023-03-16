8d007598f0237e6f436c5bb3a350ad1ea9df7d7e
Oswaldo Natera <oswaldonateram@gmail.com>
2023-03-15 19:35:53 -0500

    creando fichaproyectoformulario con su css

--- fichaProyectoFormulario.php
+++ fichaProyectoFormulario.php
@@ -5,36 +5,26 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-        <link rel="stylesheet" href="./css/fichaProyecto.css" />
+        <link rel="stylesheet" href="./css/fichaProyectoFormulario.css" />
         <link rel="stylesheet" href="./css/normalize.css" />
         <!-- Fonts -->
-        <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400;700;800&display=swap"
-            rel="stylesheet"
-        />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;1,300;1,700;1,900&display=swap" rel="stylesheet">
         <title>Formulario Ficha Proyecto</title>
     </head>
     <body>
         <header>
             <div class="logo-titulo">
-                <img
-                    src="./img/icons/Logo-de-SENA-png-verde.png"
-                    alt="logo_sena"
-                    width="80"
-                    height="80"
-                />
-                <hr />
-                <h2>
-                    Sistema de gestión de<br />
-                    proyectos formativos SENA
-                </h2>
+            <img src="./img/icons/Logo-de-SENA-png-verde.png" alt="logo_sena" width="80" height="80">
+            <hr>
+            <h2>Sistema de gestión de<br> proyectos formativos SENA</h2>
             </div>
-            <div class="usuario">
-                <span>¡Bienvenido Administrador!</span>
-                <img src="./img/icons/user.png" alt="user" />
+            <div  class="iconoHamburguesa menu" id="menu" >
+            <img src="./img/icons/hamburguesa.png" alt="logo_sena" width="30" height="30">
             </div>
         </header>
-        <nav>
+        <nav id="navega">
             <ul>
                 <li><a href="">Aprendiz</a></li>
                 <li><a href="">Participantes</a></li>
@@ -150,5 +140,6 @@
                 </form>
             </div>
         </main>
+        <script src="./js/script.js"></script>
     </body>
 </html>
