44bd773dd3f042960740b72ee4988b1e6edb4074
BryanMurillo <bryanmiessler@gmail.com>
2023-03-21 16:41:58 -0500

    puliendo detalles

--- faseAgregar.php
+++ faseAgregar.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="./css/faseAgregar.css">
+    <link rel="stylesheet" href="./css/fase.css">
     <link rel="stylesheet" href="./css/normalize.css">
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400;700;800&display=swap" rel="stylesheet">
